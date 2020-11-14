30157efd159c7b32f7fbca277d2232de1a4c50c7
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-14 19:29:20 +0100

    Finished Project 2

--- lib/live_view_studio/licenses.ex
+++ lib/live_view_studio/licenses.ex
@@ -1,9 +1,9 @@
 defmodule LiveViewStudio.Licenses do
+  def calculate(seats) when seats < 6 do
+    seats * 20
+  end
+
   def calculate(seats) do
-    if seats <= 5 do
-      seats * 20.0
-    else
-      100 + (seats - 5) * 15.0
-    end
+    100 + (seats - 5) * 15
   end
 end
